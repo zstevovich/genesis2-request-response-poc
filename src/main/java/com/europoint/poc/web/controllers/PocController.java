7b0a4c6d4e3600f6a44031762b9d8922da1126de
Zoran Stevovic <zstevovich@gmail.com>
2022-03-31 20:56:10 +0200

    Commit 1

--- src/main/java/com/europoint/poc/web/controllers/PocController.java
+++ src/main/java/com/europoint/poc/web/controllers/PocController.java
@@ -1,4 +1,14 @@
 package com.europoint.poc.web.controllers;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("PocController")
+@RequestMapping("poc")
+@AllArgsConstructor
 public class PocController {
+
+
+
 }
--- src/main/java/com/europoint/poc/web/webfluxcontroller/PocController.java
+++ src/main/java/com/europoint/poc/web/webfluxcontroller/PocController.java
@@ -1,4 +1,11 @@
 package com.europoint.poc.web.webfluxcontroller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("WebFluxPocController")
+@RequestMapping("webFlux/poc")
+@AllArgsConstructor
 public class PocController {
 }
